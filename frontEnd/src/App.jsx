a3401fc7831bb476f5ac1c4b5299c7491cf9df4b
Yashcat2 <ikykgunawardhana23072@gmail.com>
2025-05-19 03:15:15 +0530

    ui done

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 import {Routes,Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Collection from './pages/Collection'
 import About from './pages/About'
-import Conatact from './pages/Conatact'
+import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
@@ -11,16 +11,22 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import SearchBar from './components/SearchBar'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]' >
+      <ToastContainer />
       <NavBar />
+      <SearchBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
-        <Route path='/contact' element={<Conatact/>} />
+        <Route path='/contact' element={<Contact/>} />
         <Route path='/product/:productId' element={<Product/>} />
         <Route path='/cart' element={<Cart/>} />
         <Route path='/login' element={<Login/>} />
--- frontEnd/src/pages/Conatact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Conatact = () => {
-  return (
-    <div>Conatact</div>
-  )
-}
-
-export default Conatact
