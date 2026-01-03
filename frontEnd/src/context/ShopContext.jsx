72c15b579ad4fde2dbd8d12eefdcbabcf748ccc4
Yashcat2 <ikykgunawardhana23072@gmail.com>
2026-01-03 19:11:52 +0530

    toast msg corrected

--- frontEnd/src/context/ShopContext.jsx
+++ frontEnd/src/context/ShopContext.jsx
@@ -21,7 +21,7 @@ const ShopContextProvider = (props) => {
 
     const addToCart = async (itemId,size) => {
         if (!size) {
-            toast.error('Select Product Size');
+            toast.error('Select Product Core');
             return;
         }
 
